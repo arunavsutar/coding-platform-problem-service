bba808631e5c0e3d96e4b6c89b70c79884985cfb
Arunav <arunavsutar@gmail.com>
2024-03-23 16:52:53 +0530

    Feat: Added routing structure for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { PORT } = require('./config/server.config')
 const bodyParser = require('body-parser');
+const apiRouter = require('./routes');
 
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api', apiRouter);
 app.get('/ping', (req, res) => {
     return res.json({ message: "Server is Up." });
 })
